ce5ed0951cac29c56ab7ddb35c30c06c6378f9e2
David Nadlinger <code@klickverbot.at>
2024-01-12 22:10:48 +0000

    Use WignerSymbols.jl instead of SymPy
    This gets rid of the Python dependency.

--- src/rates.jl
+++ src/rates.jl
@@ -1,11 +1,11 @@
 using Unitful.DefaultSymbols
-import SymPy.Physics
+using WignerSymbols
 
 function clebsch_gordan(lower::StateSpec, upper::StateSpec)
     lo = convert(NoHyperfineNumberSpec, lower.level)
     hi = convert(NoHyperfineNumberSpec, upper.level)
     # TODO: Verify transition type.
-    sqrt(2 * hi.j + 1) * sum(Physics.wigner_3j(lo.j, 1, hi.j, -lower.m, q, upper.m) for q in -1:1) |> Float64
+    sqrt(2 * hi.j + 1) * sum(wigner3j(lo.j, 1, hi.j, -lower.m, q, upper.m) for q in -1:1) |> Float64
 end
 
 function rabi_frequency(species, lower::StateSpec, upper::StateSpec, intensity)
